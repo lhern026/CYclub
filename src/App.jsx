c8a517ecd92a040d2641bebc39c19712be6b5d01
luis hernandez <luishernandez@luiss-MacBook-Air.local>
2022-04-29 13:10:27 -0700

    yeah

--- src/App.jsx
+++ src/App.jsx
@@ -269,7 +269,7 @@ if (chainId !== rinkebyChainId) {
       </div>
     </div>
   </div>
-      <div className="container">
+      <div className="containers">
         
         
         {/* <Router>
@@ -306,19 +306,69 @@ if (chainId !== rinkebyChainId) {
         </div>
         <div>
           <h2  id="roadmap">Roadmap</h2>
-          <div className="roadmap">
-              <div className="phase1"><h1>Phase 1</h1><div className="phases"><p></p></div></div>
-              <div className="phase2"><h1>Phase 2</h1><div className="phases"><p></p></div></div>
-              <div className="phase3"><h1>Phase 3</h1><div className="phases"><p></p></div></div>
-           
-          </div>
-          <div className="roadmap">
-              <a name="roadmap"></a>
-              <div className="phase1"><h1>Phase 4</h1><div className="phases"><p></p></div></div>
-              <div className="phase2"><h1>Phase 5</h1><div className="phases"><p></p></div></div>
-              <div className="phase3"><h1>Phase 6</h1><div className="phases"><p></p></div></div>
-           
-          </div>
+          <div class="timeline">
+  <div class="container left">
+    <div class="date">15 Dec</div>
+    <i class="icon fa fa-home"></i>
+    <div class="content">
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <p>
+        Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+      </p>
+    </div>
+  </div>
+  <div class="container right">
+    <div class="date">22 Oct</div>
+    <i class="icon fa fa-gift"></i>
+    <div class="content">
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <p>
+        Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+      </p>
+    </div>
+  </div>
+  <div class="container left">
+    <div class="date">10 Jul</div>
+    <i class="icon fa fa-user"></i>
+    <div class="content">
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <p>
+        Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+      </p>
+    </div>
+  </div>
+  <div class="container right">
+    <div class="date">18 May</div>
+    <i class="icon fa fa-running"></i>
+    <div class="content">
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <p>
+        Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+      </p>
+    </div>
+  </div>
+  <div class="container left">
+    <div class="date">10 Feb</div>
+    <i class="icon fa fa-cog"></i>
+    <div class="content">
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <p>
+        Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+      </p>
+    </div>
+  </div>
+  <div class="container right">
+    <div class="date">01 Jan</div>
+    <i class="icon fa fa-certificate"></i>
+    <div class="content">
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <p>
+        Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+      </p>
+    </div>
+  </div>
+</div>
+          
           
         
               </div>
